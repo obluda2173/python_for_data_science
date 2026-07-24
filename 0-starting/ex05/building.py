95102de7d10100785256dddb3c48f1d5f58d575b
obluda2173 <obluda2173@gmail.com>
2026-07-24 16:58:17 +0200

    modify subject + ex05

---
+++ 0-starting/ex05/building.py
@@ -0,0 +1,37 @@
+import sys
+
+
+def count(text):
+    ul = ll = pm = sp = dg = 0
+    for c in text:
+        if c.isupper():
+            ul += 1
+        elif c.islower():
+            ll += 1
+        elif not c.isalnum() and not c.isspace():
+            pm += 1
+        elif c.isspace():
+            sp += 1
+        elif c.isdigit():
+            dg += 1
+    print(f"The text contains {len(text)} characters:")
+    print(f"{ul} upper letters")
+    print(f"{ll} lower letters")
+    print(f"{pm} punctuation marks")
+    print(f"{sp} spaces")
+    print(f"{dg} digits")
+
+
+def main():
+    args = sys.argv[1:]
+    assert len(args) <= 1, "more than one argument provided"
+    if not args or not args[0]:
+        print("What is the text to count?")
+        text = sys.stdin.read()
+    else:
+        text = args[0]
+    count(text)
+
+
+if __name__ == "__main__":
+    main()
