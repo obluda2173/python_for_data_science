89d10388a4496b546402c2f47684b2c53dad46f2
obluda2173 <obluda2173@gmail.com>
2026-07-24 15:08:40 +0200

    update subject + ex02 fix, ex03

--- 0-starting/ex02/find_ft_type.py
+++ 0-starting/ex02/find_ft_type.py
@@ -1,18 +1,13 @@
 def all_thing_is_obj(object: any) -> int:
+    names = {list : "List",
+             tuple : "Tuple",
+             set : "Set",
+             dict : "Dict"}
     obj_type = type(object)
-    obj_type_str = ""
-    if (obj_type == list):
-        obj_type_str = "List"
-    elif (obj_type == tuple):
-        obj_type_str = "Tuple"
-    elif (obj_type == set):
-        obj_type_str = "Set"
-    elif (obj_type == dict):
-        obj_type_str = "Dict"
+    if (obj_type in names):
+        print(f"{names[obj_type]} : {obj_type}")
     elif (obj_type == str):
-         obj_type_str = f"{object} is in the kitchen"
+        print(f"{object} is in the kitchen : {obj_type}")
     else:
         print("Type not found")
-    if obj_type_str:
-        print(f"{obj_type_str} : {type(object)}")
     return 42
---
+++ 0-starting/ex03/NULL_not_found.py
@@ -0,0 +1,15 @@
+def NULL_not_found(object: any) -> int:
+    if object is None:
+        print(f"Nothing: None {type(object)}")
+    elif type(object) is float and object != object:
+        print(f"Cheese: nan {type(object)}")
+    elif type(object) is int and object == 0:
+        print(f"Zero: 0 {type(object)}")
+    elif type(object) is str and object == "":
+        print(f"Empty: {type(object)}")
+    elif type(object) is bool and object == False:
+        print(f"Fake: False {type(object)}")
+    else:
+        print("Type not found")
+        return 1
+    return 0
