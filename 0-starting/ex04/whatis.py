43c4d2cc37e36b96cdb6dbf0872c9438d669b1ad
obluda2173 <obluda2173@gmail.com>
2026-07-24 16:13:21 +0200

    update ex04

--- 0-starting/ex04/whatis.py
+++ 0-starting/ex04/whatis.py
@@ -1,19 +1,25 @@
 import sys
 
+def is_integer(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
 def main():
     args = sys.argv[1:]
 
-    if not args:
+    if len(args) == 0:
         return
+
     try:
         assert len(args) == 1, "more than one argument is provided"
-        try:
-            num = int(args[0])
-        except ValueError:
-            raise AssertionError("argument is not an integer")
+        assert is_integer(args[0]), "argument is not an integer"
+        num = int(args[0])
         print("I'm Even." if num % 2 == 0 else "I'm Odd.")
     except AssertionError as err:
         print(f"AssertionError: {err}")
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
